f2e9f891ca9f16f10d689be26e993a194580273d
mightyflea12 <132308636+mightyflea12@users.noreply.github.com>
2023-06-08 12:06:12 -0600

    source_script.lua

--- source_script.lua
+++ source_script.lua
@@ -1,12 +1,26 @@
 local plrs = game:GetService("Players")
 
+--Grabs info (all plrs)
+
 local localplr = plrs.LocalPlayer
+
+--Grabs info of the local player (player playing)
 
 local plrgui = localplr.PlayerGui
 
+--Searches for local player gui
+
 plrgui.SpawnGui.Frame.Visible = true
 
+--Sets the spawn frame visible
+
+plrgui.SpawnGui.Open.IfOwner.Enabled = false
+
+--This makes it so it doesn't destroy itself
+
 print("SpawnGui was enabled for "..localplr.Name..".")
+
+--Prints for who the spawn frame was enabled
 
 --game is: EntityTesting 2.0 (DOORS)
 --recommended to use loadstring but u can use this
