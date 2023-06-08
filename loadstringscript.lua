b45111be0272f5e22ae7bec7a5d34dd2b0dbce7d
mightyflea12 <132308636+mightyflea12@users.noreply.github.com>
2023-06-08 11:56:24 -0600

    loadstringscript.lua

---
+++ loadstringscript.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mightyflea12/mightyflea/main/entitytesting_script.lua"))()
+--copy this script, loadstring
+--game is: EntityTesting 2.0 (DOORS)
