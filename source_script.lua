f98b1ecd65ed7e1e7e552ccde03d45b4317c735b
mightyflea12 <132308636+mightyflea12@users.noreply.github.com>
2023-06-08 11:57:28 -0600

    source_script.lua

--- source_script.lua
+++ source_script.lua
@@ -7,3 +7,6 @@ local plrgui = localplr.PlayerGui
 plrgui.SpawnGui.Frame.Visible = true
 
 print("SpawnGui was enabled for "..localplr.Name..".")
+
+--game is: EntityTesting 2.0 (DOORS)
+--recommended to use loadstring but u can use this
